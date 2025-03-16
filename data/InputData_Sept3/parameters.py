5ae0e3974a2e7b1f1a85d2056405943ada6d2d32
donsweeney4 <donsweeney4@gmail.com>
2025-03-15 18:26:34 -0700

    changed file organization

---
+++ data/InputData_Sept3/parameters.py
@@ -0,0 +1,21 @@
+# Parameters that likely change between different data sets
+
+input_directory = 'InputData_Sept3'  # Replace with your directory path
+output_directory = 'OutputData_Sept3'  # Replace with your directory path
+start_time = '2024-09-03 21:26:00'
+end_time = '2024-09-03 22:45:00'
+cuttoff_speed_MPH = 5.0
+#temperature_drift =   -0.000718 # from Alan  #-0.000487  #  Adjust temperature drift over time- based on Sensor10 @ 3m 
+temperature_drift =   -0.000487  #  Adjust temperature drift over time- based on Sensor10 @ 3m 
+color_table_min = 0.0 #19.   # enter 0.0 to use floor of minimum temperature in data
+color_table_max = 0.0 #29.   # enter 0.0 to use ceiling of maximum temperature in data
+
+# Parameters that likely don't change between different data sets
+
+solid_color_by_route = False # if true, color by route, if false, color by temperature
+color_coded_temperature_map = 'color_coded_temperature_map.html'
+color_coded_route_map = 'color_coded_route_map.html'
+temperature_map_TimeWindow = 'temperaturePlot_TimeWindow.html'
+temperature_map_entireTime = 'temperature_EntireTime.html'
+combined_data = 'combined_data.csv'
+combined_data_reduced_columns = 'combined_data_reduced_columns.csv'
---
+++ data/InputData_Sept7/parameters.py
@@ -0,0 +1,21 @@
+# Parameters that likely change between different data sets
+
+input_directory = 'InputData_Sept7'  # Replace with your directory path
+output_directory = 'OutputData_Sept7'  # Replace with your directory path
+start_time = '2024-09-07 13:41:15'
+end_time = '2024-09-07 14:37:00'
+cuttoff_speed_MPH = 5.0
+#temperature_drift =   -0.000718 # from Alan  #-0.000487  #  Adjust temperature drift over time- based on Sensor10 @ 3m 
+temperature_drift =   +.000281  #  Positive value as temperature gets hotter over time
+color_table_min = 35.35 #19.   # enter 0.0 to use floor of minimum temperature in data
+color_table_max = 37.5 #29.   # enter 0.0 to use ceiling of maximum temperature in data
+
+# Parameters that likely don't change between different data sets
+
+solid_color_by_route = False # if true, color by route, if false, color by temperature
+color_coded_temperature_map = 'color_coded_temperature_map.html'
+color_coded_route_map = 'color_coded_route_map.html'
+temperature_map_TimeWindow = 'temperaturePlot_TimeWindow.html'
+temperature_map_entireTime = 'temperature_EntireTime.html'
+combined_data = 'combined_data.csv'
+combined_data_reduced_columns = 'combined_data_reduced_columns.csv'
